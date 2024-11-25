d2609445135d9f2360d3d188486c776bdcf93917
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-11-26 02:42:32 +0330

    update(deploy on github)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="w-screen h-screen bg-gradient-to-l from-red-500 to-black  items-center  flex justify-center text-red-600  text-6xl	 font-bold">
-		<h1 className='hover:scale-110 duration-150'> !!Alpha!!</h1>
+		<h1 className='hover:scale-110 duration-150'> !!Alphabet!!</h1>
     </div>
   );
 }
