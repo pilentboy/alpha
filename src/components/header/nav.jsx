9714bf030c1bbb36ed1adfaa8d554c68346939e9
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-11-27 01:02:33 +0330

    increase nav width + add bold weight to join link

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -3,13 +3,13 @@ import { LuPhone } from "react-icons/lu";
 const Nav = () => {
   return (
     <nav>
-      <div className="w-4/5 h-14 rounded-md shadow-navShadow bg-white my-5 flex items-center justify-between px-6 py-3 mx-auto md:w-4/5 font-yekan ">
+      <div className="w-4/5 h-16 rounded-md shadow-navShadow bg-white my-5 flex items-center justify-between px-6 py-3 mx-auto md:w-[90%] font-yekan ">
         {/* left links */}
         <ul className="flex items-center space-x-3 ">
           <li>
             <Link
               to="/"
-              className="text-white rounded-md text-center bg-primary px-4 py-1 "
+              className="text-white rounded-md text-center bg-primary px-4 py-1 font-bold"
             >
               عضویت
             </Link>
