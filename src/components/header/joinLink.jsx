a5306f26b2f966945a56a1e4604a89af7b169d4f
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2024-11-27 21:16:45 +0330

    change nav position in sm screens

--- src/components/header/joinLink.jsx
+++ src/components/header/joinLink.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const JoinLink = () => (
   <Link
     to="/"
-    className="text-white rounded-md text-center bg-primary px-4 py-1 font-bold"
+    className="text-white rounded-md text-center bg-primary px-4 py-1 font-bold duration-150 hover:bg-[#23d3c7]"
   >
     عضویت
   </Link>
--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import NavLink from "./navLink";
 import JoinLink from "./joinLink";
+import Logo from "./logo";
 
 const Nav = () => (
   <nav>
-    <div className="w-4/5 h-16 rounded-md shadow-soft bg-white my-5 flex items-center justify-between px-6 py-3 mx-auto md:w-[90%] font-yekan ">
+    <div className="fixed bottom-0 left-[50%] translate-x-[-50%]  w-4/5 h-16 rounded-md shadow-soft bg-white my-5 flex items-center justify-between px-6 py-3 mx-auto md:w-[90%] font-yekan md:relative md:bottom-0 md:left-0 md:translate-x-0">
       {/* left links */}
       <ul className="flex items-center space-x-3 ">
         <li>
@@ -24,14 +25,15 @@ const Nav = () => (
           </Link>
         </li>
       </ul>
+
       {/* right links */}
       <ul className="flex flex-row-reverse items-center  text-black ">
-        <li className="mx-3 hover:text-gray-500 duration-150">
+        <li className=" hover:text-gray-500 duration-150 md:mx-3">
           <Link to="/">
             <i>
               <img
                 src={`${process.env.PUBLIC_URL}/icons/Home_Icon.png`}
-                className="w-8"
+                className="w-12 md:w-8 hover:scale-105 duration-150"
                 alt="home page"
                 title="خانه"
               />
@@ -41,6 +43,9 @@ const Nav = () => (
         <NavLink title="خدمات" target="/" />
         <NavLink title="درباره" target="/" />
       </ul>
+
+      {/* logo */}
+      <Logo />
     </div>
   </nav>
 );
